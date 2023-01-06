273ed02219a2795c4c4b8fd9dab47cddd553b358
pupuduck <ernestwongdexing@gmail.com>
2023-01-06 15:13:03 +0800

    update 1

--- models/auth/auth_forms.py
+++ models/auth/auth_forms.py
@@ -5,13 +5,13 @@ from flask_wtf import FlaskForm
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[Length(min=5, max=15), DataRequired()])
-    email = EmailField('Email', validators=[Email()])
-    password1 = PasswordField('Password', validators=[Length(min=8)])
-    password2 = PasswordField('Confirm password', validators=[Length(min=8), EqualTo(password1)])
+    email = EmailField('Email', validators=[Email(), DataRequired()])
+    password1 = PasswordField('Password', validators=[Length(min=8), DataRequired()])
+    password2 = PasswordField('Confirm password', validators=[Length(min=8), EqualTo(password1), DataRequired()])
     submit = SubmitField('Sign up')
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[Length(min=5, max=15), DataRequired()])
-    password = PasswordField('Password', validators=[Length(min=8)])
+    password = PasswordField('Password', validators=[Length(min=8), DataRequired()])
     submit = SubmitField('Log in')
